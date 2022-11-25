cf20ed97bf5fd323a5099899b2c12a9dc7d1dbde
Slider Slider <slider1595@hotmail.com>
2022-11-25 18:36:49 +0100

    Task service and observable pre API commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[]= [
+    {
+        id: 1,
+        text: 'Task 1',
+        day: '15.12.2022  at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Task 2',
+        day: '05.12.2022 at 2:30pm',
+        reminder: false,
+    },
+    {
+        id: 3,
+        text: 'Tastk 3',
+        day: '22.12.2022 at 2:30pm',
+        reminder: true,
+    }
+]
